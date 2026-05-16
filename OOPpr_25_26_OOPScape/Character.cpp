69dd259d6024b30f3516d08838647ed00ef2557e
g-grozdev <ggrozdev18@gmail.com>
2026-05-16 20:23:11 +0300

    update Character class
    updated Character class to inherit Target class instead of Damageable and Position separately, so that upscaling is possible between Hero and Target classes

--- OOPpr_25_26_OOPScape/Character.cpp
+++ OOPpr_25_26_OOPScape/Character.cpp
@@ -1,7 +1,7 @@
 #include "Character.h"
 
-Character::Character(int _hp, int _mhp, int _x, int _y, int dmg, int rng, int aoe, int _pierce) : Damageable(_hp, _mhp), 
-Position(_x, _y), wp(dmg, rng, aoe, _pierce) { }
+Character::Character(int _hp, int _mhp, int _x, int _y, int dmg, int rng, int aoe, bool _pierce) : Target(_x, _y, _hp, _mhp),
+wp(dmg, rng, aoe, _pierce) { }
 
 Weapon Character::attack(int direction)
 {
--- OOPpr_25_26_OOPScape/Character.h
+++ OOPpr_25_26_OOPScape/Character.h
@@ -4,12 +4,12 @@
 #include "Weapon.h"
 #include "IMoveable.h"
 
-class Character : public Damageable, public Position, public IMoveable
+class Character : public Target, public IMoveable
 {
 	Weapon wp;
 
 public:
-	Character(int _hp, int _mhp, int _x, int _y, int dmg, int rng, int aoe, int _pierce);
+	Character(int _hp, int _mhp, int _x, int _y, int dmg, int rng, int aoe, bool _pierce);
 	~Character() override = default;
 
 	Weapon attack(int direction);
