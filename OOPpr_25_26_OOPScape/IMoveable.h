6c4d22d893b56e7b9be976c28a5141d026751435
g-grozdev <ggrozdev18@gmail.com>
2026-05-16 20:21:57 +0300

    update IMoveable interface
    updated IMoveable interface to have the move function work with a shared pointer, since this is how the labyrinth class will store the target

--- OOPpr_25_26_OOPScape/IMoveable.h
+++ OOPpr_25_26_OOPScape/IMoveable.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <vector>
+#include <memory>
 #include "Tile.h"
 #include "Target.h"
 
 class IMoveable 
 {
 public:
-	virtual void move(const std::vector<std::vector<Tile>>& tiles, Target& tar) = 0;
+	virtual void move(const std::vector<std::vector<Tile>>& tiles, std::shared_ptr<Target>& tar) = 0;
 	virtual ~IMoveable() = default;
 };
