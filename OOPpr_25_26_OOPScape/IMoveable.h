c532c47a466b03b4f380dc1f9ae803377289b558
g-grozdev <ggrozdev18@gmail.com>
2026-05-17 16:39:55 +0300

    update IMoveable
    updated move function in IMoveable class to take in Hero pointer for enemy pathfinding function

--- OOPpr_25_26_OOPScape/IMoveable.h
+++ OOPpr_25_26_OOPScape/IMoveable.h
@@ -7,6 +7,6 @@
 class IMoveable 
 {
 public:
-	virtual void move(const std::vector<std::vector<Tile>>& tiles, std::shared_ptr<Target>& tar) = 0;
+	virtual void move(const std::vector<std::vector<Tile>>& tiles, std::shared_ptr<Target>& tar, std::shared_ptr<Hero> hero) = 0;
 	virtual ~IMoveable() = default;
 };
